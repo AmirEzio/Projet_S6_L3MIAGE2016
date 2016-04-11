445f16b73de918e9b15bbabe124128813c1ba978
AmirEzio <romani.amir@gmail.com>
2016-04-11 11:05:28 +0200

    creation du rmi

---
+++ FrameWork/src/rmi/Information.java
@@ -0,0 +1,9 @@
+package rmi;
+
+import java.rmi.*;
+
+public interface Information extends Remote {
+
+   public String getInformation() throws RemoteException;
+
+}
---
+++ FrameWork/src/rmi/LanceServeur.java
@@ -0,0 +1,39 @@
+package rmi;
+
+
+
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.UnknownHostException;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+public class LanceServeur {
+
+  public static void main(String[] args) {
+    try {
+      LocateRegistry.createRegistry(1099);
+
+      System.out.println("Mise en place du Security Manager ...");
+      if (System.getSecurityManager() == null) {
+        System.setSecurityManager(new RMISecurityManager());
+      }
+
+      InformationImpl informationImpl = new InformationImpl();
+
+      String url = "rmi://" + InetAddress.getLocalHost().getHostAddress() + "/TestRMI";
+      System.out.println("Enregistrement de l'objet avec l'url : " + url);
+      Naming.rebind(url, informationImpl);
+
+      System.out.println("Serveur lancé");
+    } catch (RemoteException e) {
+      e.printStackTrace();
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+}
